61f78aa8b1de4a77cf3e57f32e1cfd6104d5ac27
Florian Bury <bury@hectronic.com>
2024-03-07 13:31:06 +0100

    ADD Not upscaling images

--- internal/imgHandler/resize.go
+++ internal/imgHandler/resize.go
@@ -12,6 +12,14 @@ func ResizeImage(src_path string, dst_path string, width int) (string, error) {
 	srcImage, err := imaging.Open(src_path, imaging.AutoOrientation(true))
 	dstImage800 := imaging.Resize(srcImage, width, 0, imaging.Linear)
 
+	bnds := srcImage.Bounds()
+
+	width_o := bnds.Size().X
+	if width_o >= width_o {
+		log.Info("Requested image bigger than original. nor rescaling. oWidth: ", width_o, " new width ", width)
+		return src_path, nil
+	}
+
 	// Create the file to which the resized image will be written.
 	out, err := os.Create(dst_path)
 	if err != nil {
